cc5bad2375eef60c114dcc84a6057bb7d4262070
[Kemmie001] <[Oluwakemisola757@gmail.com]>
2020-10-31 16:45:55 +0100

    Thank you page and firebase

---
+++ script.js
@@ -0,0 +1,32 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA3yZHHCz8XCpnniTlHIh-fSefKy73JEpo",
+    authDomain: "coming-soon-7f020.firebaseapp.com",
+    databaseURL: "https://coming-soon-7f020.firebaseio.com",
+    projectId: "coming-soon-7f020",
+    storageBucket: "coming-soon-7f020.appspot.com",
+    messagingSenderId: "1065766586683",
+    appId: "1:1065766586683:web:28b146eba15651281c4f1e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var firestore = firebase.firestore();
+
+
+const submitBtn = document.getElementById('submitBtn')
+
+
+let email = document.getElementById('email');
+
+const database = firestore.collection("BinaryData")
+
+submitBtn.addEventListener("click", function(e){
+
+    let userEmail = email.value;
+
+    // To acess the database collection
+    database.doc().set({
+        Email: userEmail
+    })
+  
+    document.getElementById("contactform").reset()
+})
